d308be18be5c201afb4412b792d297188f7e63c0
System Administrator <root@Nabils-MBP.lan>
2024-03-19 17:53:25 -0700

    Fixed custom header and sub color and styles.

--- src/app/_models/Tag.ts
+++ src/app/_models/Tag.ts
@@ -4,7 +4,7 @@ export class Tag {
     static readonly PYTHON = new Tag ('Python', 'pink');
     static readonly CSHARP = new Tag ('C#', 'green');
     static readonly JAVA = new Tag ('Java', 'orange');
-    static readonly NODEJS = new Tag ('Node.Js', 'brown');
+    static readonly NODEJS = new Tag ('Node.Js', 'lightgreen');
     static readonly ASPNET = new Tag ('ASP.NET', 'purple');
     static readonly JAVASCRIPT = new Tag ('JavaScript', 'orange');
     static readonly REACT = new Tag ('React', 'blue');
