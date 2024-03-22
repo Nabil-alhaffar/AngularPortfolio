775e09564734b3355a467ee57c9b2822e5ce4d29
System Administrator <root@Nabils-MBP.lan>
2024-03-21 22:53:51 -0700

    Added amplify directories

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,10 @@ import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
 import { importProvidersFrom } from '@angular/core';
 import { BrowserAnimationsModule, provideAnimations } from '@angular/platform-browser/animations';
+import { Amplify } from 'aws-amplify';
+import amplifyconfig  from './amplifyconfiguration.json';
+
+Amplify.configure(amplifyconfig);
 
 bootstrapApplication(AppComponent, appConfig)
   .catch((err) => console.error(err));
